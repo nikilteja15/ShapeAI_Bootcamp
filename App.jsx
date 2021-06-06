549079e40c3219ece5084c65d06142bb45381524
nikilteja15 <85442051+nikilteja15@users.noreply.github.com>
2021-06-06 18:13:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>BattleGrounds</h1>
+      <p>
+        PlayerUnknown's Battlegrounds (PUBG) is an online multiplayer battle
+        royale game developed and published by PUBG Corporation{" "}
+      </p>
+    </div>
+  );
+}
+export default Note;
